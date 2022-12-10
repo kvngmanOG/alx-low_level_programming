42ab7ccb65a30431396e0c93e55b070a1ad3b16f
kvngmanOG <justoghosa@gmail.com>
2022-12-09 20:26:37 -0800

    nmbrz

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,19 +1,19 @@
 #include <stdio.h>
 /**
- * main  main block
- * d
+ * main - main block
+ * Description: Base ten
  * Return: o
  */
 int main(void)
 {
 	int c = 0;
-	
+
 	while (c < 10)
 	{
 		putchar(48 + c);
 		c++;
 	}
-	
+
 	putchar('\n');
 	return (0);
 }
