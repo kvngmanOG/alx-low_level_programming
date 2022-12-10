45981689026779978db5b64d5eb2b22bb70cea47
kvngmanOG <justoghosa@gmail.com>
2022-12-09 19:30:45 -0800

    alph 3

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - main block
+ * description: get rand
+ * Return: o
+ */
+int main(void)
+{
+	char c ='a';
+
+	while (c <= 'z')
+	{
+		if (c != 'e' && c != 'q')
+		{
+			putchar(c);
+		}
+		c++;
+	}
+	putchar('\n');
+	return (0);
+}
