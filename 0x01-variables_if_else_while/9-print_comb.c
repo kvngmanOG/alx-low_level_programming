6ca7991eabe44ebabdffcd51a151ad3b5037c5e2
kvngmanOG <justoghosa@gmail.com>
2022-12-12 05:30:03 -0800

    comb

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - main block
+ * Description: combmination
+ * Return: o
+ */
+int main(void)
+{
+	int c = 0;
+
+	while ( c < 10)
+	{
+		putchar(48 + c);
+		if (c != 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+		c++;
+	}
+	putchar('\n');
+	return (0);
+}
