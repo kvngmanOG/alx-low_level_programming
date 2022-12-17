00d764dee87faf6fc37f3abf86e19615459736ea
kvngmanOG <justoghosa@gmail.com>
2022-12-16 18:42:15 -0800

    100-prime_factor.c

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -1,8 +1,8 @@
 #include "main.h"
 /**
  * print_triangle - check for a digit
- * @size : integer type
- * Return:void
+ * @size: integer type
+ * Return: void
  */
 void print_triangle(int size)
 {
