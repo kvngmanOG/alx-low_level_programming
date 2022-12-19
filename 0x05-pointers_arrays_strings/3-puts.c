7427fd7418fb4656d39d368de67fbc3954b6fd75
kvngmanOG <justoghosa@gmail.com>
2022-12-19 08:16:30 -0800

    3-puts.c

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -2,16 +2,20 @@
 
 /**
  * _puts - function prints string to stdout
+ *
  * @str: input string
+ *
  */
 
 void _puts(char *str)
 {
 int index;
 
-for (index = 0; str[index] != '\0'; index++)
+for (index = 0; str[index] != '\0' ; index++)
+
 {
-_putchar(str[index]);
+	_putchar(str[index]);
 }
 _putchar('\n');
+
 }
