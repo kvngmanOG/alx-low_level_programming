55f3df65a19476faf7aa6e014a30546a66bc5faa
kvngmanOG <justoghosa@gmail.com>
2022-12-19 13:58:32 -0800

    7-puts_half.c

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * puts_half - prints half a string
+ * @str: input
+ */
+
+void puts_half(char *str)
+{
+int index, half;
+
+index = 0;
+while (str[index] != '\0')
+index++;
+
+half = index / 2;
+if (index % 2 == 1)
+half++;
+
+while (half < index)
+{
+putchar(str[half]);
+half++;
+}
+
+putchar('\n');
+}
