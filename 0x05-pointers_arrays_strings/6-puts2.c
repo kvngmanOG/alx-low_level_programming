6bce6f286d6e2c591991f16184cd988ab652dc3a
kvngmanOG <justoghosa@gmail.com>
2022-12-19 13:38:24 -0800

    6-puts2.c

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * puts2 - prints other characters
+ * @str: pointer
+ * Return: void
+ */
+
+void puts2(char *str)
+{
+int i;
+
+i = 0;
+while (str[i] != '\0')
+{
+if (i % 2 ==0)
+{
+putchar(str[i]);
+}
+i++;
+}
+putchar('\n');
+
+}
