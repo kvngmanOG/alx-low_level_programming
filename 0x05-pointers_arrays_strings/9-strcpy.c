7a63505258151af8495e00d8899654bc48a494a4
kvngmanOG <justoghosa@gmail.com>
2022-12-19 14:39:01 -0800

    9-strcpy.c

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * _strcpy - copy variable
+ * @dest: destination
+ * @src: copier
+ * Return: the cpoy
+ */
+
+void _strcpy(char *dest, char *src)
+{
+char *start = dest;
+
+while (*src != '\0')
+{
+*dest = *src;
+dest++;
+src++;
+}
+*dest = '\0';
+
+return (start);
+}
