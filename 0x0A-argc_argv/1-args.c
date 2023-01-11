cccff26cf7f46d43c72177519d109735f79b382f
kvngmanOG <justoghosa@gmail.com>
2023-01-11 04:04:23 -0800

    t1

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - program that prints numbers
+ * @args: count
+ * @argv: vector
+ * Return: 0
+ */
+int main(int args, char *argv[])
+{
+printf("%d\n", args - 1);
+(void)argv;
+return (0);
+}
