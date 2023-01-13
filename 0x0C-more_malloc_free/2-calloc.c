08bef5a25ab801913b1367c5fe2a327caa223510
kvngmanOG <justoghosa@gmail.com>
2023-01-13 09:03:32 -0800

    t2 a

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -11,7 +11,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 void *p;
 unsigned int i;
 
-if (nmemb == 0 || size === 0)
+if (nmemb == 0 || size == 0)
 return (NULL);
 p = malloc(nmemb * size);
 if (p == NULL)
@@ -20,7 +20,7 @@ return (NULL);
 }
 for (i = 0; i < (nmemb * size); i++)
 {
-((char *)(p) + i) = 0;
+*((char *)(p) +i) = 0;
 }
 return (p);
 }
