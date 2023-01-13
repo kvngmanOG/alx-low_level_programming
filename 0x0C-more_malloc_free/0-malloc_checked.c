cb8956fd1f5cbe66785c4125294e255b3c296238
kvngmanOG <justoghosa@gmail.com>
2023-01-13 01:33:34 -0800

    t0

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+/**
+ * malloc_checked - allocates memory
+ * @b: size
+ * Return: pointer
+ */
+void *malloc_checked(unsigned int b)
+{
+void *p = malloc(b);
+if (p == NULL)
+exit(98);
+
+return (p);
+}
