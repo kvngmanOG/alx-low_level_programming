3499a0f928af66ebb83b83c62001843f3958c2a4
kvngmanOG <justoghosa@gmail.com>
2023-01-20 07:03:41 -0800

    t3

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,61 @@
+#include "3-calc.h"
+/**
+ * op_add - return the sum
+ * @a: integer
+ * @b: integer
+ * return: sum
+ */
+int op_add(int a, int b)
+{
+return (a + b);
+}
+/**
+ * op_sub - returns diff 
+ * @a: integer
+ * @b: integer
+ * return: diff
+ */
+int op_sub(int a, int b)
+{
+return (a - b);
+}
+/**
+ * op_mul - returns product
+ * @a: integer
+ * @b: integer
+ * return: product
+ */
+int op_mul(int a, int b)
+{
+return (a * b);
+}
+/**
+ * op_div - returns the quotient
+ * @a: integer
+ * @b: integer
+ * return: quotient
+ */
+int op_div(int a, int b)
+{
+if (b == 0)
+{
+printf("Error\n");
+exit(100);
+}
+return (a / b);
+}
+/**
+ * op_mod - returns the remainder of division
+ * @a: integer
+ * @b: integer
+ * return: remainder
+ */
+int op_mod(int a, int b)
+{
+if (b == 0)
+{
+printf("Error\n");
+exit(100);
+}
+return (a % b);
+}
