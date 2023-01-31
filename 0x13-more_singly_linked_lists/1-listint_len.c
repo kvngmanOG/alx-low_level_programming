b905e21ebaf33ace1f26a165c7eddef41e3795b9
kvngmanOG <justoghosa@gmail.com>
2023-01-31 05:44:51 -0800

    t1

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+#include <stdio.h>
+
+/**
+ * listint_len - returns the number of elements
+ * @h: pointer
+ * Return: the number
+ */
+size_t listint_len(const listint_t *h)
+{
+size_t node = 0;
+
+while (h)
+{
+nodes++;
+h = h->next;
+}
+return (nodes);
+}
